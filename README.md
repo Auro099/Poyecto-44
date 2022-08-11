652e19ac0f128d5156e08b732fa02d7cf520aeb2
Auro <98856226+Auro099@users.noreply.github.com>
2022-08-11 15:50:09 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Poyecto-44
+# p5.play-plantilla
+Plantilla para p5.play
---
+++ sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
+Una niña llamada Regina que no tenia materiales para hacer apuntes bonitos entonces 
+un día la invitaron a un concurso de quien podia recolectar las mejores
+ cosas para el regreso a clases entonces Regina decidió participar y 
+ agarrar cosas bonitas para su regreso a clases pero sabia que tal vez 
+ perdia  haci que lo tenia que hacer con mucho cuidado.El juego se trataba 
+ de intentar agarrar las mejores cosas para el regreso a clases porque si agarraba 
+ lo feo perdia.
+
+ PC: Playing Characters (Personajes jugables)
+ Solo se podra jugar en PC
+ Tendra un sonido cada que agarre cosas bonitas.
